77b1e4d1410911721e2f5ebd47ac235f7f510da4
alex <hello@back48off48.ru>
2019-07-15 16:26:17 +0400

    1

--- themes/default/bootstrap.php
+++ themes/default/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Theme bootstrap file. 48
+ * Theme bootstrap file. 48888
  */
 Yii::app()->getComponent('bootstrap');
 
@@ -8,10 +8,5 @@ Yii::app()->getClientScript()->registerScript('baseUrl', "var baseUrl = '" . Yii
 $address=preg_replace('/<span>[a-zA-Zа-яА-Я\\s,\\d\\.]+<\\/span>/u', '', Yii::app()->getModule('yupe')->siteAddress, -1, $count);
 Yii::app()->getClientScript()->registerScript('map-scheme', "var scheme_address = '" . $address . "';", CClientScript::POS_HEAD);
 Yii::app()->getClientScript()->registerLinkTag('shortcut icon', null, Yii::app()->getTheme()->getAssetsUrl() . '/images/favicon.ico');
-
-
-<<<<<<< HEAD
 Yii::import('themes.'.Yii::app()->theme->name.'.DefautThemeEvents');
-=======
 Yii::import('themes.'.Yii::app()->theme->name.'.DefautThemeEvents'); 
->>>>>>> remotes/origin/verstka_dtp
