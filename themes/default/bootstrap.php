c371b326ecc4e3a81923695233ade476b8a5f90d
alex <hello@back48off48.ru>
2019-07-15 16:38:17 +0400

    1

--- themes/default/bootstrap.php
+++ themes/default/bootstrap.php
@@ -7,6 +7,7 @@ Yii::app()->getComponent('bootstrap');
 Yii::app()->getClientScript()->registerScript('baseUrl', "var baseUrl = '" . Yii::app()->getBaseUrl(true) . "';", CClientScript::POS_HEAD);
 $address=preg_replace('/<span>[a-zA-Zа-яА-Я\\s,\\d\\.]+<\\/span>/u', '', Yii::app()->getModule('yupe')->siteAddress, -1, $count);
 Yii::app()->getClientScript()->registerScript('map-scheme', "var scheme_address = '" . $address . "';", CClientScript::POS_HEAD);
+Yii::app()->getClientScript()->registerScript('map-scheme2', "var scheme_address = '" . $address . "';", CClientScript::POS_HEAD);
 Yii::app()->getClientScript()->registerLinkTag('shortcut icon', null, Yii::app()->getTheme()->getAssetsUrl() . '/images/favicon.ico');
 Yii::import('themes.'.Yii::app()->theme->name.'.DefautThemeEvents');
 Yii::import('themes.'.Yii::app()->theme->name.'.DefautThemeEvents'); 
